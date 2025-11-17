06dd10ab3b1047abdc0699fc3dcbe9b9d3157848
Sayed-Rafat-Ali <148480225+Sayed-Rafat-Ali@users.noreply.github.com>
2025-11-18 02:04:33 +0530

    Initial commit

--- src/main/java/com/myBank/bankProject/config/DB.java
+++ src/main/java/com/myBank/bankProject/config/DB.java
@@ -7,9 +7,15 @@ public class DB {
 	
 
 
-	    private static final String URL = "jdbc:mysql://localhost:3306/bankdb";
-	    private static final String USER = "root";
-	    private static final String PASS = "#Rafat1410";
+//	    private static final String URL = "jdbc:mysql://localhost:3306/bankdb";
+//	    private static final String USER = "root";
+//	    private static final String PASS = "#Rafat1410";
+	
+	private static final String URL =
+	        "jdbc:mysql://mysql-rafatbank.alwaysdata.net:3306/rafatbank_23?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+
+	private static final String USER = "rafatbank";
+	private static final String PASS = "#Rafat1410";
 
 	    public static Connection getConnection() {
 	        try {
